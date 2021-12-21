77c0d0d21d5d935b4fce39581c2de3092f90b405
lijianhao <1498158052@qq.com>
2021-12-21 22:37:03 +0800

    更改typescript问题

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,7 +1,7 @@
 import Hooks from './hooks'
 import Utils from './utils'
 
-declare module 'react-rough-utils' {
+declare module 'rough-react-utils' {
     export const Hooks: Hooks
     export const Utils: Utils
 }
